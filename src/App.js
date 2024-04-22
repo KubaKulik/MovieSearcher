ca9a02b2030c98d72d57f3b5b12e3b01417fff7a
Sebastian Kowalski <wwy.skowalski@wp.pl>
2024-04-22 14:07:14 +0200

    Dodanie elementu films do nawigacji oraz poprawione style

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Routes, Route } from 'react-router-dom';
 import Nav from "./components/views/Nav/Nav";
 import Login from './components/Pages/Login/Login';
 import Register from './components/Pages/Register/Register';
+import { Axios } from 'axios';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Routes>
         <Route path="/Login/" element={<Login />} />
         <Route path="/Register/*" element={<Register />} />
+        <Route path="/Films/*"/>
       </Routes>
     </main>
   );
--- src/components/views/Nav/Nav.js
+++ src/components/views/Nav/Nav.js
@@ -1,11 +1,12 @@
 import { NavLink } from 'react-router-dom';
-
+import styles from './Nav.module.scss';
 
 const Nav = () => {
   return(
-    <nav>
-      <NavLink to="/Login/"> Login </NavLink>
-      <NavLink to="/Register/"> Register </NavLink>
+    <nav className={styles.nav}>
+      <NavLink className={styles.navItem} to="/Login/"> Login </NavLink>
+      <NavLink className={styles.navItem} to="/Register/"> Register </NavLink>
+      <NavLink className={styles.navItem} to="/Films/"> Films </NavLink>
     </nav >
   );
 };
