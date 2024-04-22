d53a4aa8df55e3487e95b763945f159bb353b9ad
Gabriela <gabriela.rostek@gmail.com>
2024-04-22 11:45:01 +0200

    dodanie logowania i rejestracji - błąd styli

--- src/components/Pages/Login/Login.js
+++ src/components/Pages/Login/Login.js
@@ -1,7 +1,19 @@
 import styles from './Login.module.scss';
+
 const Login = () => {
   return (
-    <div className={styles.login}> Logowanie </div>
+    <div className={styles.login}>
+      <h2>Logowanie</h2>
+      <form className={styles.loginForm} action="/login" method="post">
+        <label htmlFor="username">Nazwa użytkownika:</label>
+        <input type="text" id="username" name="username" required />
+
+        <label htmlFor="password">Hasło:</label>
+        <input type="password" id="password" name="password" required />
+
+        <input type="submit" value="Zaloguj się" />
+      </form>
+    </div>
   );
 };
 
--- src/components/Pages/Register/Register.js
+++ src/components/Pages/Register/Register.js
@@ -1,7 +1,26 @@
+
 import styles from './Register.module.scss';
+
 const Register = () => {
   return (
-    <div className={styles.register}> Register </div>
+    <div className={styles.register}>
+      <h2>Rejestracja</h2>
+      <form className={styles.registerForm} action="/submit" method="post">
+        <label htmlFor="username">Nazwa użytkownika:</label>
+        <input type="text" id="username" name="username" required />
+
+        <label htmlFor="email">Adres email:</label>
+        <input type="email" id="email" name="email" required />
+
+        <label htmlFor="password">Hasło:</label>
+        <input type="password" id="password" name="password" required />
+
+        <label htmlFor="confirm_password">Potwierdź hasło:</label>
+        <input type="password" id="confirm_password" name="confirm_password" required />
+
+        <input type="submit" value="Zarejestruj się" />
+      </form>
+    </div>
   );
 };
 
