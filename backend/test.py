47d0de7118272d8ee7191e5a4d880225e989109b
Thizz00 <jakubkielb7@gmail.com>
2024-05-28 11:21:42 +0200

    Unit and integration tests added 😎

--- backend/test.py
+++ backend/test.py
@@ -1,4 +1,5 @@
 import requests
+import pytest
 import uuid
 
 BASE_URL = "http://localhost:8000"
@@ -12,48 +13,42 @@ def generate_random_email():
 def generate_random_password():
     return "password123"
 
-def test_register():
+
+@pytest.fixture
+def new_user():
     username = generate_random_username()
     email = generate_random_email()
     password = generate_random_password()
     user_data = {"username": username, "email": email, "password": password}
     response = requests.post(f"{BASE_URL}/register", json=user_data)
     assert response.status_code == 200
-    assert response.json()["message"] == "user created successfully"
+    return {"username": username, "password": password}
 
-def test_login():
-    username = generate_random_username()
-    password = generate_random_password()
-    user_data = {"username": username, "email": generate_random_email(), "password": password}
-    response = requests.post(f"{BASE_URL}/register", json=user_data)
-    assert response.status_code == 200
+@pytest.fixture
+def logged_in_user(new_user):
+    username = new_user["username"]
+    password = new_user["password"]
     login_data = {"username": username, "password": password}
     response = requests.post(f"{BASE_URL}/login", json=login_data)
     assert response.status_code == 200
-    assert "access_token" in response.json()
-    assert "refresh_token" in response.json()
     return response.json()["access_token"]
 
-def test_forgot_password():
-    username = generate_random_username()
-    password = generate_random_password()
-    user_data = {"username": username, "email": generate_random_email(), "password": password}
-    response = requests.post(f"{BASE_URL}/register", json=user_data)
-    assert response.status_code == 200
+def test_register(new_user):
+    assert new_user is not None
+
+def test_login(logged_in_user):
+    assert logged_in_user is not None
+
+def test_forgot_password(new_user):
+    username = new_user["username"]
     new_password = generate_random_password()
     password_data = {"username": username, "new_password": new_password, "repeat_new_password": new_password}
     response = requests.put(f"{BASE_URL}/forgot-password", json=password_data)
     assert response.status_code == 200
     assert response.json()["message"] == "Password changed successfully"
 
-def test_logout():
-    access_token = test_login()
-    headers = {"Authorization": f"Bearer {access_token}"}
+def test_logout(logged_in_user):
+    headers = {"Authorization": f"Bearer {logged_in_user}"}
     response = requests.post(f"{BASE_URL}/logout", headers=headers)
     assert response.status_code == 200
     assert response.json()["message"] == "Logout Successfully"
-
-if __name__ == "__main__":
-    test_register()
-    test_login()
-    test_forgot_password()
